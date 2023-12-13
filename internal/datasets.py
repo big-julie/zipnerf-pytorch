0b41975ad3902869007e54db8d061d8d7ad50541
big-julie <151593169+big-julie@users.noreply.github.com>
2023-12-13 14:18:46 +0800

    Read COLMAP as default.

--- internal/datasets.py
+++ internal/datasets.py
@@ -537,12 +537,12 @@ class LLFF(Dataset):
             factor = 1
 
         # Copy COLMAP data to local disk for faster loading.
-        colmap_dir = os.path.join(self.data_dir, 'odometry')
+        colmap_dir = os.path.join(self.data_dir, 'sparse/0/')
 
         # Load poses.
         if utils.file_exists(colmap_dir):
-            pixtocam , image_names, poses, distortion_params, camtype = load_llff_posedata(colmap_dir)
-            # image_names, poses, pixtocam, distortion_params, camtype = NeRFSceneManager(colmap_dir).process()
+            # pixtocam , image_names, poses, distortion_params, camtype = load_llff_posedata(colmap_dir)
+            image_names, poses, pixtocam, distortion_params, camtype = NeRFSceneManager(colmap_dir).process()
         else:
             # # Attempt to load Blender/NGP format if COLMAP data not present.
             # pose_data = load_blender_posedata(self.data_dir)
